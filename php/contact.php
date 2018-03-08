cf0544b59deb0b89b1e763c3c6d2bd2c0c5bf283
[spencer lynch] <[spencer.lynch9@yahoo.com]>
2018-03-08 17:11:16 -0500

    working through FAQ and contact sections

--- php/contact.php
+++ php/contact.php
@@ -6,17 +6,25 @@ $field_email = $_POST['email'];
 
 $field_phone = $_POST['phone'];
 
+$field_street = $_POST['street'];
+
+$field_zip = $_POST['zip'];
+
 $field_message = $_POST['message'];
 
-$mail_to = 'to@email.com';
+$mail_to = 'spencer.lynch@workwithspencer.com';
 
-$subject = 'Message from a site visitor '.$field_first_name;
+$subject = 'Free Quote Inquiry '.$field_first_name;
 
 $body_message = 'From: '.$field_first_name."\n";
 
 $body_message .= 'E-mail: '.$field_email."\n";
 
 $body_message .= 'Phone: '.$field_phone."\n";
+
+$body_message .= 'Street Address: '.$field_street."\n";
+
+$body_message .= 'Zip: '.$field_zip."\n";
 
 $body_message .= 'Message: '.$field_message;
 
@@ -29,14 +37,14 @@ $mail_status = mail($mail_to, $subject, $body_message, $headers);
 
 if ($mail_status) { ?>
 	<script language="javascript" type="text/javascript">
-		//alert('Thank you for the message. We will contact you shortly.');
+		alert('Thank you for the message. We will contact you shortly.');
 		window.location = 'index.html';
 	</script>
 <?php
 }
 else { ?>
 	<script language="javascript" type="text/javascript">
-		//alert('Message failed. Please, send an email to gordon@template-help.com');
+		alert('Message failed. Please, send an email to fightthebite@bgmosquitozero.com');
 		window.location = 'index.html';
 	</script>
 <?php

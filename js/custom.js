a0ecd48af45bc83232f5bd404d90a1680e190ece
[spencer lynch] <[spencer.lynch9@yahoo.com]>
2018-04-04 18:17:06 -0400

    temporary contact form, goes straight to spam

--- js/custom.js
+++ js/custom.js
@@ -103,26 +103,55 @@ $(window).on('load', function(){
 	/* ========================================================== */
 	/*   Contact                                                  */
 	/* ========================================================== */
-	$('#contact-form').each( function(){
-		var form = $(this);
-		//form.validate();
-		form.submit(function(e) {
-			if (!e.isDefaultPrevented()) {
-				jQuery.post(this.action,{
-					'names':$('input[name="contact_names"]').val(),
-					'subject':$('input[name="contact_subject"]').val(),
-					'email':$('input[name="contact_email"]').val(),
-					'phone':$('input[name="contact_phone"]').val(),
-					'message':$('textarea[name="contact_message"]').val(),
-				},function(data){
-					form.fadeOut('fast', function() {
-						$(this).siblings('p').show();
-					});
-				});
-				e.preventDefault();
-			}
-		});
-	})
+	$("#my_form").submit(function(event){
+    event.preventDefault(); //prevent default action 
+    var proceed = true;
+    var form = this;
+    
+    //simple validation at client's end
+    //loop through each field and we simply change border color to red for invalid fields       
+    $(form).find(':required').each(function(){
+        $(this).css('border-color',''); 
+        if(!$.trim($(this).val())){ //if this field is empty 
+            $(this).css('border-color','red'); //change border color to red   
+            proceed = false; //set do not proceed flag
+        }
+        //check invalid email
+        var email_reg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/; 
+        if($(this).attr("type")=="email" && !email_reg.test($.trim($(this).val()))){
+            $(this).css('border-color','red'); //change border color to red   
+            proceed = false; //set do not proceed flag              
+        }   
+
+    }).keyup(function() { //reset previously set border colors on .keyup()
+        $(this).css('border-color',''); 
+    }).change(function() {  //for select box
+        $(this).css('border-color',''); 
+    }); 
+    
+    if(proceed){ //everything looks good! proceed...
+        //get input field values data to be sent to server
+        var post_url = $(this).attr("action"); //get form action url
+        var request_method = $(this).attr("method"); //get form GET/POST method
+        var form_data = $(this).serialize(); //Encode form elements for submission
+        
+        //Ajax post data to server
+        $.ajax({
+            url : post_url,
+            type: request_method,
+            dataType : 'json',
+            data : form_data
+        })
+        .done(function(response){ 
+            if(response.type == 'error'){ //load json data from server and output message     
+                output = '<div class="error">'+response.text+'</div>';
+            }else{
+                $(form)[0].reset(); //reset this form upon success
+                output = '<div class="success">'+response.text+'</div>';
+            }
+            $("#contact_form #contact_results").html(output);
+        });
+    }
 
 	
 });

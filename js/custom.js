acf74c38a678da1f1e10826adf8e3f92fa94cc74
[spencer lynch] <[spencer.lynch9@yahoo.com]>
2018-04-15 19:39:13 -0400

    adding in images and editing contact form still

--- js/custom.js
+++ js/custom.js
@@ -85,48 +85,58 @@ $(window).on('load', function(){
 	/* ========================================================== */
 	/*   Contact                                                  */
 	/* ========================================================== */
-	// Input Lock
-	$('textarea').blur(function () {
-	    $('#hire textarea').each(function () {
-	        $this = $(this);
-	        if ( this.value != '' ) {
-	          $this.addClass('focused');
-	          $('textarea + label + span').css({'opacity': 1});
-	        }
-	        else {
-	          $this.removeClass('focused');
-	          $('textarea + label + span').css({'opacity': 0});
-	        }
-	    });
-	});
+	$(document).ready(function() {
+    $("#submit_btn").click(function() { 
+       
+	    var proceed = true;
+        //simple validation at client's end
+        //loop through each field and we simply change border color to red for invalid fields		
+		$("#contact_form input[required], #contact_form textarea[required]").each(function(){
+			$(this).css('background-color',''); 
+			if(!$.trim($(this).val())){ //if this field is empty 
+				$(this).css('background-color','#FFDEDE'); //change border color to #FFDEDE   
+				proceed = false; //set do not proceed flag
+			}
+			//check invalid email
+			var email_reg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/; 
+			if($(this).attr("type")=="email" && !email_reg.test($.trim($(this).val()))){
+				$(this).css('background-color','#FFDEDE'); //change border color to #FFDEDE   
+				proceed = false; //set do not proceed flag				
+			}	
+		});
+       
+        if(proceed) //everything looks good! proceed...
+        {
+			//get input field values data to be sent to server
+            POST_data = {
+				'user_name'		: $('input[name=name]').val(), 
+				'user_email'	: $('input[name=email]').val(), 
+				'subject'		: $('input[name=subject]').val(), 
+				'msg'			: $('textarea[name=message]').val()
+			};
+            
+            //Ajax post data to server
+            $.POST('php/sendmail.php', POST_data, function(response){  
+				if(response.type == 'error'){ //load json data from server and output message     
+					output = '<div class="error">'+response.text+'</div><br>';
+				}else{
+				    output = '<div class="success">'+response.text+'</div><br>';
+					//reset values in all input fields
+					$("#contact_form  input[required=true], #contact_form textarea[required=true]").val(''); 
+					$("#contact_form #contact_body").slideUp(); //hide form after success
+				}
+				$("#contact_form #contact_results").hide().html(output).slideDown();
+            }, 'json');
+        }
+    });
+    
+    //reset previously set border colors and hide all message on .keyup()
+    $("#contact_form  input[required=true], #contact_form textarea[required=true]").keyup(function() { 
+        $(this).css('background-color',''); 
+        $("#result").slideUp();
+    });
+});
 
-	$('#hire .field:first-child input').blur(function () {
-	    $('#hire .field:first-child input').each(function () {
-	        $this = $(this);
-	        if ( this.value != '' ) {
-	          $this.addClass('focused');
-	          $('.field:first-child input + label + span').css({'opacity': 1});
-	        }
-	        else {
-	          $this.removeClass('focused');
-	          $('.field:first-child input + label + span').css({'opacity': 0});
-	        }
-	    });
-	});
-
-	$('#hire .field:nth-child(2) input').blur(function () {
-	    $('#hire .field:nth-child(2) input').each(function () {
-	        $this = $(this);
-	        if ( this.value != '' ) {
-	          $this.addClass('focused');
-	          $('.field:nth-child(2) input + label + span').css({'opacity': 1});
-	        }
-	        else {
-	          $this.removeClass('focused');
-	          $('.field:nth-child(2) input + label + span').css({'opacity': 0});
-	        }
-	    });
-	});
 	/* ========================================================== */
 	/*   Animated-Features                                        */
 	/* ========================================================== */
@@ -145,3 +155,5 @@ $(window).on('load', function(){
      	previous.removeClass('active'); // previous list-item
      	$(e.target).addClass('active'); // activated list-item
    		});
+    
+});
--- php/contact_me.php
+++ php/contact_me.php
@@ -3,61 +3,59 @@ if($_POST)
 {
     $to_email       = "spencer.lynch@workwithspencer.com"; //Recipient email, Replace with own email here
     
-    //check if its not an ajax request just exit!
-    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
-        print "Can't access directly!";
-        exit;
-    } 
+    //check if its an ajax request, exit if not
+    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+        
+        $output = json_encode(array( //create JSON data
+            'type'=>'error', 
+            'text' => 'Sorry Request must be Ajax POST'
+        ));
+        die($output); //exit script outputting json data
+    }
     
     //Sanitize input data using PHP filter_var().
-    $sender_Name    = filter_var($_POST["user_Name"], FILTER_SANITIZE_STRING);
-    $sender_Email   = filter_var($_POST["user_Email"], FILTER_SANITIZE_EMAIL);
-    $phone_Number   = filter_var($_POST["phone_Number"], FILTER_SANITIZE_NUMBER_INT);
+    $user_name      = filter_var($_POST["user_name"], FILTER_SANITIZE_STRING);
+    $user_email     = filter_var($_POST["user_email"], FILTER_SANITIZE_EMAIL);
     $subject        = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
-    $message        = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+    $message        = filter_var($_POST["msg"], FILTER_SANITIZE_STRING);
     
     //additional php validation
-    if(strlen($sender_Name)<3){ // If length is less than 4 it will output JSON error.
-        $output = json_encode(array('type'=>'error', 'text' => 'Name is too short or empty!'));
+    if(strlen($user_name)<2){ // If length is less than 4 it will output JSON error.
+        $output = json_encode(array('type'=>'error', 'text' => '<p>Name is too short or empty!</p>'));
         die($output);
     }
-    if(!filter_var($sender_Email, FILTER_VALIDATE_EMAIL)){ //email validation
-        print json_encode(array('type'=>'error', 'text' => 'Please enter a valid email!'));
-        exit;
+    if(!filter_var($user_email, FILTER_VALIDATE_EMAIL)){ //email validation
+        $output = json_encode(array('type'=>'error', 'text' => '<p>Please enter a valid email!</p>'));
+        die($output);
     }
-    if(!filter_var($phone_Number, FILTER_SANITIZE_NUMBER_FLOAT)){ //check for valid numbers in phone number field
-        print json_encode(array('type'=>'error', 'text' => 'Enter only digits in phone number'));
-        exit;
+    if(strlen($subject)<3){ //check emtpy subject
+        $output = json_encode(array('type'=>'error', 'text' => '<p>Subject is required</p>'));
+        die($output);
     }
     if(strlen($message)<3){ //check emtpy message
-        print json_encode(array('type'=>'error', 'text' => 'Too short message! Please enter something.'));
-        exit;
+        $output = json_encode(array('type'=>'error', 'text' => '<p>Too short message! Please enter something.</p>'));
+        die($output);
     }
     
-	
- 	//email body
-    $message_body =  "Message from $sender_Name\n";
-    $message_body .=  "------------------------------\n";
-    $message_body .=  "$message\n";
-    $message_body .=  "------------------------------\n";
-    $message_body .=  "$sender_name\n";
-    $message_body .=  "$sender_email\n";
-    $message_body .=  "$phone_Number\n";
-	    
+    //email body
+    $message_body = $message."\r\n\r\n-".$user_name."\r\nEmail : ".$user_email;
+    
     //proceed with PHP email.
-    $headers = 'From: '. $sender_Name, .'' . "\r\n" .
-    'Reply-To: '.$sender_Email.'' . "\r\n" .
+    $headers = 'From: '.$user_name.'' . "\r\n" .
+    'Reply-To: '.$user_email.'' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
     
-    $send_mail = mail($to_email, $sender_Name, $subject, $message_body, $headers);
+    $send_mail = mail($to_email, $subject, $message_body, $headers);
     
-    if(!$send_mail){
-        //If mail couldn't be sent, this is probably server's fault, check your mail configuration or consult your host
-        print json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration or consult your web host.'));
-        exit;
+    if(!$send_mail)
+    {
+        //If mail couldn't be sent output error. Check your PHP email configuration (if it ever happens)
+        $output = json_encode(array('type'=>'error', 'text' => '<p>Could not send mail! Please check your PHP mail configuration.</p>'));
+        die($output);
     }else{
-        print json_encode(array('type'=>'message', 'text' => 'Hi '. $sender_Name.' Thank you for your email, we will be in touch soon!'));
-        exit;
+        $output = json_encode(array('type'=>'message', 'text' => '<div class="alert alert-success" role="alert">
+        Hi '.$user_name .', Thank you very much for your message,we will contact you soon.</div>'));
+        die($output);
     }
 }
 ?>

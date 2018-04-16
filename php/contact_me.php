305bd304a20dc288037f5e084a30fdc0a9b0b68d
[spencer lynch] <[spencer.lynch9@yahoo.com]>
2018-04-16 18:33:39 -0400

    contact form is working, goes to spam though

--- php/contact_me.php
+++ php/contact_me.php
@@ -1,61 +1,68 @@
 <?php
 if($_POST)
 {
-    $to_email       = "spencer.lynch@workwithspencer.com"; //Recipient email, Replace with own email here
+    $to_email       = "theaveragespencer@gmail.com"; //Recipient email, Replace with own email here
+    $from_email     = 'noreply@your_domain.com'; //from mail, it is mandatory with some hosts and without it mail might endup in spam.
     
-    //check if its an ajax request, exit if not
-    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
-        
-        $output = json_encode(array( //create JSON data
-            'type'=>'error', 
-            'text' => 'Sorry Request must be Ajax POST'
-        ));
-        die($output); //exit script outputting json data
-    }
+    //check if its not an ajax request just exit!
+    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
+        print "Can't access directly!";
+        exit;
+    } 
     
     //Sanitize input data using PHP filter_var().
-    $user_name      = filter_var($_POST["user_name"], FILTER_SANITIZE_STRING);
-    $user_email     = filter_var($_POST["user_email"], FILTER_SANITIZE_EMAIL);
+    $sender_name    = filter_var($_POST["user_name"], FILTER_SANITIZE_STRING);
+    $sender_email   = filter_var($_POST["user_email"], FILTER_SANITIZE_EMAIL);
+    $phone_number   = filter_var($_POST["phone_number"], FILTER_SANITIZE_NUMBER_INT);
     $subject        = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
-    $message        = filter_var($_POST["msg"], FILTER_SANITIZE_STRING);
+    $message        = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
     
     //additional php validation
-    if(strlen($user_name)<2){ // If length is less than 4 it will output JSON error.
-        $output = json_encode(array('type'=>'error', 'text' => '<p>Name is too short or empty!</p>'));
+    if(strlen($sender_name)<3){ // If length is less than 4 it will output JSON error.
+        $output = json_encode(array('type'=>'error', 'text' => 'Name is too short or empty!'));
         die($output);
     }
-    if(!filter_var($user_email, FILTER_VALIDATE_EMAIL)){ //email validation
-        $output = json_encode(array('type'=>'error', 'text' => '<p>Please enter a valid email!</p>'));
-        die($output);
+    if(!filter_var($sender_email, FILTER_VALIDATE_EMAIL)){ //email validation
+        print json_encode(array('type'=>'error', 'text' => 'Please enter a valid email!'));
+        exit;
+    }
+    if(!filter_var($phone_number, FILTER_SANITIZE_NUMBER_FLOAT)){ //check for valid numbers in phone number field
+        print json_encode(array('type'=>'error', 'text' => 'Enter only digits in phone number'));
+        exit;
     }
     if(strlen($subject)<3){ //check emtpy subject
-        $output = json_encode(array('type'=>'error', 'text' => '<p>Subject is required</p>'));
-        die($output);
+        print json_encode(array('type'=>'error', 'text' => 'Subject is required'));
+        exit;
     }
     if(strlen($message)<3){ //check emtpy message
-        $output = json_encode(array('type'=>'error', 'text' => '<p>Too short message! Please enter something.</p>'));
-        die($output);
+        print json_encode(array('type'=>'error', 'text' => 'Too short message! Please enter something.'));
+        exit;
     }
     
-    //email body
-    $message_body = $message."\r\n\r\n-".$user_name."\r\nEmail : ".$user_email;
     
+    //email body
+    $message_body =  "Message from $sender_name\n";
+    $message_body .=  "------------------------------\n";
+    $message_body .=  "$message\n";
+    $message_body .=  "------------------------------\n";
+    $message_body .=  "$sender_name\n";
+    $message_body .=  "$sender_email\n";
+    $message_body .=  "$phone_number\n";
+        
     //proceed with PHP email.
-    $headers = 'From: '.$user_name.'' . "\r\n" .
-    'Reply-To: '.$user_email.'' . "\r\n" .
+    $headers = 'From: '. $sender_name .'' . "\r\n" .
+    'Reply-To: '.$sender_email.'' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
     
     $send_mail = mail($to_email, $subject, $message_body, $headers);
     
-    if(!$send_mail)
-    {
-        //If mail couldn't be sent output error. Check your PHP email configuration (if it ever happens)
-        $output = json_encode(array('type'=>'error', 'text' => '<p>Could not send mail! Please check your PHP mail configuration.</p>'));
-        die($output);
+    if(!$send_mail){
+        //If mail couldn't be sent, this is probably server's fault, check your mail configuration or consult your host
+        print json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration or consult your web host.'));
+        exit;
     }else{
-        $output = json_encode(array('type'=>'message', 'text' => '<div class="alert alert-success" role="alert">
-        Hi '.$user_name .', Thank you very much for your message,we will contact you soon.</div>'));
-        die($output);
+        print json_encode(array('type'=>'message', 'text' => 'Hi '. $sender_name.' Thank you for your email'));
+        exit;
     }
 }
 ?>
